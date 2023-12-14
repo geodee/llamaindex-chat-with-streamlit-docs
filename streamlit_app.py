ad39027aaa817a2f10c7024a682521e9278f84cd
George Dedopoulos <george@cilantric.com>
2023-12-13 20:19:25 -0500

    kkk

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from llama_index import VectorStoreIndex, ServiceContext, Document
-from llama_index.llms import OpenAI
+from llama_index import GPTVectorStoreIndex, ServiceContext, Document
+from llama_index.embeddings.openai import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
 
